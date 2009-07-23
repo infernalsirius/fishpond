1f781396d6c2d419c405307f37cc60412fb4986e
Patrick <patrick@lssupport1.local>
2009-07-22 20:06:30 -0400

    Added models. Html work

--- app/controllers/hardwares_controller.rb
+++ app/controllers/hardwares_controller.rb
@@ -1,5 +1,60 @@
 class HardwaresController < ApplicationController
   def index
     @allhardware = Hardware.find(:all)
+    
+    respond_to do |wants|
+      wants.html
+    end
   end
+  
+  def show
+    @harware = Hardware.find(params[:id])
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def new
+    @hardware = Hardware.new
+  end
+  
+  def edit
+    @hardware = Hardware.find(params[:id])
+  end
+  
+  def create
+    @hardware = Hardware.new(params[:hardware])
+    
+    respond_to do |wants|
+      if @hardware.save
+        flash[:notice] = 'Le matériel a été sauvegardé avec succès'
+        wants.html
+      else
+        wants.html { render new_hardware_path }
+      end
+    end
+  end
+  
+  def update
+    @hardware = Hardware.find(params[:id])
+    
+    respond_to do |wants|
+      if @hardware.update_attributes(params[:hardware])
+        wants.html          
+      else
+        wants.html { render edit_hardware_path }
+      end
+    end
+  end
+  
+  def destroy
+    @hardware = Hardware.find(params[:id])
+    @hardware.destroy
+    
+    respond_to do |wants|
+      wants.html { redirect_back_or_default }
+    end
+  end
+  
 end
