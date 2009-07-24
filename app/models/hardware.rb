b35a13a0b24dc675a6fa3d6e6286d714300da587
Patrick <patrick@lssupport1.local>
2009-07-23 23:45:04 -0400

    Work on hardware panels

--- app/models/hardware.rb
+++ app/models/hardware.rb
@@ -3,4 +3,5 @@ class Hardware < ActiveRecord::Base
   
   validates_uniqueness_of :serial, :mac, :idNum, 
           :message => "Le numéro de série, l'adresse MAC ou le numéro d'identification du matériel a déjà été entré."
+  validates_presence_of :idNum, :serial, :maker, :on => :create, :message => "Doit être saisie"
 end
