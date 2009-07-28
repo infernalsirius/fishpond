093d2dc5c62d2ba030c79744529bf095ee5d0b22
Patrick <patrick@lssupport1.local>
2009-07-27 20:09:27 -0400

    Added validations in models and created the equivalent migration.

---
+++ db/migrate/20090723235130_add_association_field.rb
@@ -0,0 +1,19 @@
+class AddAssociationField < ActiveRecord::Migration
+  def self.up
+    add_column :licenses, :hardware_id, :integer
+    add_column :licenses, :server_id, :integer
+    add_column :accessories, :server_id, :integer
+    add_column :accessories, :harware_id, :integer
+    add_column :hardwares, :location_id, :integer
+    add_column :servers, :location_id, :integer
+  end
+
+  def self.down
+    remove_column :servers, :location_id
+    remove_column :hardwares, :location_id
+    remove_column :accessories, :harware_id
+    remove_column :accessories, :column_name
+    remove_column :table_name, :column_name
+    remove_column :hardwares, :licence_id
+  end
+end
