e9b9d9a53786dc96be5816c2d7a2b7aa906319cb
infernalsirius <patrick@pat.local>
2009-09-04 22:05:10 -0400

    Added color-striped rows in list and modified the way errors shows on form submission.

--- config/environment.rb
+++ config/environment.rb
@@ -43,3 +43,11 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+# This is for the forms error.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance| 
+  if html_tag =~ /<label/
+    %|<div class="fieldWithErrors">#{html_tag} <span class="error">#{[instance.error_message].join(', ')}</span></div>|
+  else
+    html_tag
+  end
+end
