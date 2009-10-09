9dd8a43f7fdef96c12fecf615dca3083cf15cba0
infernalsirius <patrick@pat.local>
2009-10-08 21:54:48 -0400

    Added Maker views and methods.

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,4 +1,14 @@
-Class MakersController < ApplicationController
+class MakersController < ApplicationController
+  before_filter :require_user
+  layout "dark"
+  
+  def index
+    @makers = Maker.paginate(:per_page => 10, :page => params[:page])
+    
+    respond_to do |wants|
+      wants.html {  }
+    end
+  end
   
   def new
     @maker = Maker.new 
@@ -7,7 +17,24 @@ Class MakersController < ApplicationController
   def create
     @maker = Maker.new(params[:maker])
     
+    respond_to do |wants|
+      if @maker.save
+        flash[:notice] = "Le fabriquant a été sauvegardé avec succès"
+        wants.html { redirect_back_or_default(new_hardware_path) }
+      else
+        flash[:notice] = "Le fabriquant n'a pas été sauvegardé"
+        wants.html { render new_maker_path, :layout => 'dark' }
+      end
+    end
+  end
+  
+  def destroy
+    @maker = Maker.find(params[:id])
     
+    @maker.destroy
     
+    respond_to do |wants|
+      wants.html { redirect_to makers_path }
+    end
   end
 end
--- app/models/maker.rb
+++ app/models/maker.rb
@@ -1,3 +1,6 @@
 class Maker < ActiveRecord::Base
   has_many :hardwares
+  
+  validates_uniqueness_of :name, :on => :create, :message => " est déjà utilisé."
+  validates_presence_of :name, :on => :create, :message => "ne peut être vide."
 end
