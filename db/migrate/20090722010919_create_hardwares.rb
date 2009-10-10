4b9d3cd13c44e07ba3ea746b3206eb8dfa1aa601
infernalsirius <patrick@pat.local>
2009-10-09 23:27:11 -0400

    fix: typo in migration.

--- db/migrate/20090722010919_create_hardwares.rb
+++ db/migrate/20090722010919_create_hardwares.rb
@@ -4,7 +4,7 @@ class CreateHardwares < ActiveRecord::Migration
       t.integer :idNum, :maker_id, :poste_id, :critical_id, :department_id, 
                 :operating_system_id, :model_number_id, :location_id, :ram,
                 :disk1, :disk2,
-      t.string :mac1, :mac2, :serial, :fonction, 
+      t.string :mac1, :mac2, :serial, :fonction
       t.timestamps
     end
     add_index :hardwares, :idNum
