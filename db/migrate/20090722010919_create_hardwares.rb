206d976fd0d1fd34fd9dcdbcf631b8269ccde806
infernalsirius <patrick@pat.local>
2009-10-16 22:47:22 -0400

    FIX: IdNum should be alphanumeric.

--- db/migrate/20090722010919_create_hardwares.rb
+++ db/migrate/20090722010919_create_hardwares.rb
@@ -1,10 +1,10 @@
 class CreateHardwares < ActiveRecord::Migration
   def self.up
     create_table :hardwares do |t|
-      t.integer :idNum, :maker_id, :poste_id, :critical_id, :department_id, 
+      t.integer :maker_id, :poste_id, :critical_id, :department_id, 
                 :operating_system_id, :model_number_id, :location_id, :ram,
                 :disk1, :disk2
-      t.string :mac1, :mac2, :serial, :fonction
+      t.string :mac1, :mac2, :serial, :fonction, :idNum
       t.timestamps
     end
     add_index :hardwares, :idNum
