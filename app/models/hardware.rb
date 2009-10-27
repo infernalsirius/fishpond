46a6d8c7d76ba2ce64c65a45049d478fe4f75f8a
infernalsirius <patrick@pat.local>
2009-10-26 20:39:56 -0400

    FIX: Temporarly got rid of some model validations.

--- app/models/hardware.rb
+++ app/models/hardware.rb
@@ -7,7 +7,7 @@ class Hardware < ActiveRecord::Base
   
   validates_uniqueness_of :serial, :on => :create, :message => "doit être unique"
   validates_uniqueness_of :idNum, :on => :create, :message => "doit être unique"
-  validates_uniqueness_of :mac, :on => :create, :message => "doit être unique"
-  validates_presence_of :idNum, :serial, :maker, :department, :on => :create, :message => "obligatoire"
+  validates_uniqueness_of :mac1, :on => :create, :message => "doit être unique"
+  validates_presence_of :idNum, :serial, :maker, :on => :create, :message => "obligatoire"
   
 end
