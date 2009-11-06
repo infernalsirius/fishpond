056bb9d713e70a1f5f17b1354c5122b84c4ec10d
infernalsirius <patrick@pat.local>
2009-11-05 23:56:10 -0500

    Installed version 2  of Hoptoad Notifer plugin

--- app/controllers/model_numbers_controller.rb
+++ app/controllers/model_numbers_controller.rb
@@ -13,7 +13,7 @@ class ModelNumbersController < ApplicationController
     respond_to do |wants|
       if @modelNumber.save!
         flash[:notice] = "Le numéro de modèle à été sauvegarder avec succès."
-        wants.html { redirect_back_or_default(new_model_number_path) }
+        wants.html { redirect_back_or_default(new_hardware_path) }
       else
         flash[:notice] = "Le numéro de modèle n'a pu être sauvegardé."
         wants.html { render new_model_number_path, :layout => "dark" }
