064222d43552e97b915cc687939fbdc99c3f5725
infernalsirius <patrick@pat.local>
2009-11-20 23:47:53 -0500

    Generated Lockdown plugin to add role functionality.

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -5,7 +5,7 @@ class LicensesController < ApplicationController
   layout 'dark'
   
   def index
-    @licenses = License.paginate(:per_page => 10, :page => params[:page])
+    @licenses = License.paginate(:per_page => 15, :page => params[:page])
     
     respond_to do |wants|
       wants.html
